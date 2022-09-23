dc9650821c62e3dc23cdd048569148b379ce9e4b
xuanwu4 <113288222+xuanwu4@users.noreply.github.com>
2022-09-22 22:35:13 -0700

    Update drowsap.js

--- scripts/drowsap.js
+++ scripts/drowsap.js
@@ -11,14 +11,25 @@ function drowsap(){
       page.push({name:"(12-25-0977) Email Notification", link:"intel/entity"});
    }
    else if(i === "with friend or flock beside them"){page.push({name:"Personnel File", link:"personnel/879206e6f742073656c6c20746865206169"});}
-   else if(i === "on wings of ash and bone"){page.push({name:"Personnel File", link:"personnel/0746865206772656174207365612c206173"});}
+   else if(i === "on wings of ash and bone"){
+      page.push({name:"Personnel File", link:"personnel/0746865206772656174207365612c206173"});
+      page.push({name:"Diversion Testfile 01", link:"1"});
+      page.push({name:"Diversion Testfile 02", link:"2"});
+      page.push({name:"Diversion Testfile 03", link:"3"});
+      page.push({name:"Diversion Testfile 04", link:"4"});
+      page.push({name:"Diversion Testfile 05", link:"5"});
+      page.push({name:"Diversion Testfile 06", link:"6"});
+      page.push({name:"Diversion Testfile 07", link:"7"});
+      page.push({name:"Diversion Testfile 08", link:"8"});
+      page.push({name:"Diversion Testfile 09", link:"9"});
+   }
    else if(i === "over its augurs"){page.push({name:"Personnel File", link:"personnel/617420537069726974206d616b652074686"});}
    else if(i === "only on tuesdays"){page.push({name:"Personnel File", link:"personnel/83f20446964206e6f742074686520477265"});}
    else if(i === "no differently than the hawk"){page.push({name:"Personnel File", link:"personnel/56d20616c6c20666f722074686520757365"});}
    if(page.length != 0){
       out += "<p style=\"color:Chartreuse;\">Access Granted.</p>\n";
       for(let i = 0; i < page.length; i++){
-         out += "<a href=\"" + page[i].link + "\" style=\"color:White;\">>  "+page[i].name+"</a><br>\n";
+         out += "<a href=\"" + page[i].link + "\" style=\"color:White;\">>  "+page[i].name+"</a><br \\>\n";
       }
    }
    document.getElementById("result").innerHTML = out;
